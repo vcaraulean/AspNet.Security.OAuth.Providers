2b9e6d7fe976cfcc8ab7e6974fd9598687b4a9de
Valeriu Caraulean <valeriu@caraulean.com>
2019-10-27 20:39:09 +0100

    Drafting Meetup OAuth flow
    Used MailChimp as a model.
    Handler implementation is missing.

---
+++ src/AspNet.Security.OAuth.Meetup/MeetupAuthenticationHandler.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System.Text.Encodings.Web;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace AspNet.Security.OAuth.Meetup
+{
+    public class MeetupAuthenticationHandler : OAuthHandler<MeetupAuthenticationOptions>
+    {
+        public MeetupAuthenticationHandler(
+            [NotNull] IOptionsMonitor<MeetupAuthenticationOptions> options,
+            [NotNull] ILoggerFactory logger,
+            [NotNull] UrlEncoder encoder,
+            [NotNull] ISystemClock clock)
+            : base(options, logger, encoder, clock)
+        {
+        }
+    }
+}
