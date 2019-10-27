51545a253509f6f33db21d44a114bf9172655a09
Valeriu Caraulean <valeriu@caraulean.com>
2019-10-27 21:27:27 +0100

    Unit test for Meetup auth flow

---
+++ test/AspNet.Security.OAuth.Providers.Tests/Meetup/MeetupTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AspNet.Security.OAuth.Meetup
+{
+    public class MeetupTests : OAuthTests<MeetupAuthenticationOptions>
+    {
+        public MeetupTests(ITestOutputHelper outputHelper)
+        {
+            OutputHelper = outputHelper;
+        }
+
+        public override string DefaultScheme => MeetupAuthenticationDefaults.AuthenticationScheme;
+
+        protected internal override void RegisterAuthentication(AuthenticationBuilder builder)
+        {
+            builder.AddMeetup(options => ConfigureDefaults(builder, options));
+        }
+
+        [Theory]
+        [InlineData(ClaimTypes.NameIdentifier, "my-id")]
+        [InlineData(ClaimTypes.Name, "John Smith")]
+        [InlineData(ClaimTypes.Email, "john@john-smith.local")]
+        public async Task Can_Sign_In_Using_Meetup(string claimType, string claimValue)
+        {
+            // Arrange
+            using (var server = CreateTestServer())
+            {
+                // Act
+                var claims = await AuthenticateUserAsync(server);
+
+                // Assert
+                AssertClaim(claims, claimType, claimValue);
+            }
+        }
+    }
+}
